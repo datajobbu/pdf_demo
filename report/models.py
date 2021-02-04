9145e33ba5b4869040f650bbaf66164d86310cb2
hojuly <hojune.choi.me@gmail.com>
2021-02-04 10:04:07 +0900

    02 모델 생성 & 등록

--- report/models.py
+++ report/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Report(models.Model):
+    code = models.CharField(max_length = 5)
+    title = models.CharField(max_length = 30)
+    contents = models.TextField()
